9d90d558a0d6aa96c65d170b0894d6f180772b9c
Fogzan <91115262+Fogzan@users.noreply.github.com>
2022-04-17 13:02:43 +0300

    Add files via upload

---
+++ Server/functionsforserver.cpp
@@ -0,0 +1,50 @@
+/*!
+ * \file
+ * \brief Файл с имплементацией функций, заданных в functionsforserver.h
+ */
+
+#include "functionsforserver.h"
+#include "DataBase.h"
+
+QByteArray parsing (QString command, qintptr socketDescriptor) {
+        QStringList parameters = command.split(QLatin1Char(' '));
+        QString function = parameters.at(0);
+        parameters.pop_front();
+
+        if (function == "auth") {
+            if (parameters.length() == 2) return auth(parameters.at(0), parameters.at(1), socketDescriptor);
+            else return "Wrong parameters amount (Need 2). Check 'auth' documentation.\n";
+        }
+        else if (function == "reg") {
+            if (parameters.length() == 3) return reg(parameters.at(0), parameters.at(1), parameters.at(2), socketDescriptor);
+            else return "Wrong parameters amount (Need 3). Check 'reg' documentation.\n";
+        }
+        else return "Wrong syntax. Check for available commands.\n";
+}
+
+
+QByteArray auth(QString login, QString password, qintptr socketDescriptor)
+{
+    // auth emulation:
+
+    // check for user in database; if exist:
+    // push socketDescriptor in database in "logged in users" table (?)
+    QByteArray result = "";
+    result.append(DataBase::TestLogAndPas(login, password).toUtf8());
+//    DataBase::closeDB();
+    return result;
+    // else return message "unable to log in"
+}
+
+QByteArray reg(QString login, QString password, QString email, qintptr socketDescriptor)
+{
+    // register emulation:
+
+    // check for user in database; if not exist:
+    // push parameters in "users" table; call "auth" func
+    QByteArray result = "";
+    result.append(DataBase::AddLogAndPas(login, password, email).toUtf8());
+//    DataBase::closeDB();
+    return result;
+    // else return message "user already exist/not valid parameters"
+}
---
+++ Server/mytcpserver.h
@@ -0,0 +1,43 @@
+/*!
+ * \file
+ * \brief Заголовочный файл с описанием класса MyTcpServer
+ */
+
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+
+#include "mytcpthread.h"
+#include <QObject>
+#include <QTcpServer>
+#include <QtNetwork>
+#include <QDebug>
+#include <QTcpSocket>
+
+/*!
+ * \brief MyTcpServer - класс, описывающий работу с сервером.
+ * \details Класс MyTcpServer используется для инициализации TCP сервера, прослушивания и приема входящих подключений.
+ */
+class MyTcpServer : public QTcpServer
+{
+    Q_OBJECT
+public:
+    /*!
+     * \brief MyTcpServer - конструктор класса MyTcpServer
+     * \details Инициализирует сервер и устанавливает прослушивание входящих подключений с любого адреса к порту 33333
+     * \param [in] parent - родительский объект QObject
+     */
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+protected:
+    /*!
+     * \brief incomingConnection - переопределенная виртуальная функция класса QTcpServer
+     * \details Вызывается при наличии входящего подключения. Инициализирует новый поток MyTcpThread и передает ему автоматически сгенерированный дескриптор для дальнейшего создания и работы с сокетом
+     * \param [in] socketDescriptor - автоматически генерируемый дескриптор сокета
+     */
+    void incomingConnection(qintptr socketDescriptor);
+//    QTcpSocket getSocketByDescriptor(qintptr desc);
+private:
+//    QMap<qintptr, QTcpSocket> *socketList;
+};
+
+#endif // MYTCPSERVER_H
