d10e98332c2c23b59cce77040241520cbb9cac8a
Fogzan <91115262+Fogzan@users.noreply.github.com>
2022-04-25 05:32:07 +0300

    Add statistics

---
+++ Server/functionsforserver.h
@@ -0,0 +1,46 @@
+/*!
+ * \file
+ * \brief Заголовочный файл, определяющий функции сервера для взаимодействия с клиентом
+ */
+
+#ifndef FUNCTIONSFORSERVER_H
+#define FUNCTIONSFORSERVER_H
+
+#include <QStringList>
+#include <QString>
+
+
+/*!
+ * \brief parsing - функция, выполняющая парсинг сообщений со стороны клиента. Данная функция выделяет команды, параметры и аргументы из строки клиента и вызывает соответствующие функции, передавая в них параметры, полученные от клиента.
+ * \param [in] command - строка, содержащая сообщение, отправленное клиентом
+ * \param [in] socketDescriptor - дескриптор сокета клиента, от которого было получено сообщение
+ * \return Возвращает результат работы функции, вызываемой клиентом
+ * \todo Обработка исключений Qt, не поддерживаемая использованием try/catch
+ * \todo Замена дерева if/else более эффективным методом проверки комманд
+ */
+QByteArray parsing(QString data_from_client, qintptr socketDescriptor);
+
+/*!
+ * \brief auth - функция, выполняющая [проверку существования аккаунта] и аутентификацию пользователя
+ * \param [in] login - логин, полученный из строки сообщения пользователя
+ * \param [in] password - пароль, полученный из строки сообщения пользователя
+ * \param [in] socketDescriptor - дескриптор сокета клиента, совершающего аутентификацию
+ * \return Возвращает сообщение с результатом аутентификации пользователя
+ */
+QByteArray auth(QString login, QString password, qintptr socketDescriptor);
+
+/*!
+ * \brief reg - функция, выполняющая [проверку существования аккаунта] и регистрацию пользователя
+ * \param [in] login - создаваемый логин, полученный из строки сообщения пользователя
+ * \param [in] password - создаваемый пароль, полученный из строки сообщения пользователя
+ * \param [in] email - электронная почта, полученная из строки сообщения пользователя
+ * \param [in] socketDescriptor - дескриптор сокета клиента, совершающего регистрацию
+ * \return Возвращает сообщение с результатом регистрации пользователя
+ */
+QByteArray reg(QString login, QString password, QString email, qintptr socketDescriptor);
+
+QByteArray ReadStatistics(QString login, qintptr socketDescriptor);
+
+QByteArray TestAnswer(QString login, QString numberQuestion, QString condition, QString answer, qintptr socketDescriptor);
+
+#endif // FUNCTIONSFORSERVER_H
---
+++ Server/mytcpthread.cpp
@@ -0,0 +1,50 @@
+/*!
+ * \file
+ * \brief Файл, содержащий имплементацию методов класса MyTcpThread
+ */
+
+#include "mytcpthread.h"
+
+MyTcpThread::~MyTcpThread() {
+    requestInterruption();
+    wait();
+    if (DEBUG_ENV) qDebug() << "Thread instance terminated: client " << this->socketDescriptor;
+}
+
+MyTcpThread::MyTcpThread(qintptr threadDesc, QObject *parent) : QThread{parent} {
+    this->socketDescriptor = threadDesc;
+}
+
+void MyTcpThread::run() {
+    socket = new QTcpSocket();
+    socket->setSocketDescriptor(this->socketDescriptor);
+
+
+    //Qt::DirectConnection used to emit readyRead slot immidiately on signal call
+    connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()), Qt::DirectConnection);
+    connect(socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
+
+    if (DEBUG_ENV) qDebug() << "Client connected, descriptor: " << this->socketDescriptor;
+
+    exec();
+}
+
+// to fix: readAll() works incorrectly with PuTTY telnet client
+void MyTcpThread::readyRead() {
+    if (socket->bytesAvailable()>0) {
+        QByteArray command = socket->readAll();
+        if (command.length() > 0) {
+//            command.remove(command.size()-2,2); //для Putty
+            socket->write(parsing(command, socket->socketDescriptor()));
+            if (DEBUG_ENV) qDebug() << "Client " << socket->socketDescriptor() << " sent message: " << command;
+        }
+    }
+}
+
+void MyTcpThread::disconnected() {
+    if (DEBUG_ENV) qDebug() << "Client disconnected, descriptor: " << this->socketDescriptor;
+
+    socket->deleteLater();
+    exit(0);
+    // remove socketDescriptor from "logged in users" table in database (?)
+}
